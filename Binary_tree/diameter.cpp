740e6dbf898ada508cdff174d3577e5e95d7b584
fucking ajeet <ajeet808080@gmail.com>
2023-02-22 23:25:26 +0530

    updates

--- Binary_tree/diameter.cpp
+++ Binary_tree/diameter.cpp
@@ -15,31 +15,18 @@ struct Node
     }
 };
 
+int maxi =0;
+ 
 
 
-int dfs(Node* root)
+
+int diam(Node* root,maxi)
 {
      if(root == NULL )
      return 0;
 
-     int l = dfs(root->left);
-     if(l == -1)
-        return -1;
 
-     int r = dfs(root -> right);
-     if(r == -1)
-       return -1;
-     
-
-     if(abs(l-r)>1)
-       return -1;
-
-     return max(l,r)+1;
-}
-bool check(Node* root)
-{ 
-    return dfs(root)!=-1;
-    
+     int l = diam()
 }
 
 
