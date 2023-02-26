333306c4258cfb00dfc3497cb3efb4c6dd1bcae8
fucking ajeet <ajeet808080@gmail.com>
2023-02-26 17:03:03 +0530

    update

--- Binary_tree/maximum-pathsum.cpp
+++ Binary_tree/maximum-pathsum.cpp
@@ -20,6 +20,18 @@ struct Node
 };
 
 
+int maxpath(Node* root, int &maxi)
+{
+    if(root == NULL)
+     return 0;
+
+    int l = maxpath(root->left, maxi);
+    int r = maxpath(root->right, maxi);
+
+    maxi = max(maxi, root->data+l+r);
+
+    return root->data + max(l,r);
+}
 
 
 
@@ -30,12 +42,16 @@ struct Node
 
 int main() 
 {
+     int maxi = 0;
+
+     
 	struct Node* root = new Node(1);
     root -> left = new Node(2);
     root -> right = new Node(3);
     root -> right -> left = new Node(4);
     root -> right -> right = new Node(5);
     root -> right -> left -> left = new Node(6);
-
+    int sum = maxpath(root, maxi);
+    cout<<sum<<endl;
 	return 0;
 }
