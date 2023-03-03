9ca108f87fb918d3f13d0807826746f5801eaa13
fucking ajeet <ajeet808080@gmail.com>
2023-03-04 00:46:41 +0530

    update

--- Binary_tree/boundary-traversal.cpp
+++ Binary_tree/boundary-traversal.cpp
@@ -16,19 +16,118 @@ struct Node
     
 };
 
+bool isleaf(Node* root)
+{
+    return (!root->left && !root -> right);
+}
+
+void addleft(Node* root, vector<int> &ans)
+{
+    Node* curr = root -> left;
+    while(curr)
+    {
+         if(!isleaf(curr))
+         {
+             ans.push_back(curr -> data);
+         }
+         if(curr -> left)
+         {
+            curr = curr -> left;
+         }else{
+            curr = curr -> right;
+         }
+    }
+     
+}
+
+void addright(Node* root, vector<int> &ans)
+{
+    vector<int> temp;
+    Node* curr = root -> right;
+    while(curr)
+    {
+         if(!isleaf(curr))
+         {
+            temp.push_back(curr->data);
+         }
+         if(curr -> right)
+         {
+              curr = curr -> right;
+         }else {
+            curr = curr -> left;
+         }
+    }
+
+    for(int i = temp.size()-1; i>=0; --i)
+    {
+         ans.push_back(temp[i]);
+    }
+
+     
+}
+
+void addleave(Node* root , vector<int> &ans)
+{
+
+    if(isleaf(root))
+    {
+        ans.push_back(root->data);
+        return;
+    }
+
+    if(root -> left )
+      addleave(root->left, ans);
+    if(root -> right)
+      addleave(root->right, ans);
+     
+}
+
+vector<int> boundary(Node* root)
+{
+     vector<int> ans;
+      if(!root)
+      return ans;
+   
+      if(!isleaf(root))
+      {
+         ans.push_back(root->data);
+      }
+
+      addleft(root, ans);
+      addleave(root, ans);
+      addright(root, ans);
+
+      return ans;
+}
+
 
 
 
 
 int main() 
 {
-    int maxi = 0; 
-	struct Node* root = new Node(1);
-    root -> left = new Node(2);
-    root -> right = new Node(3);
-    root -> left -> left = new Node(4);
-    root -> left -> right = new Node(5);
-    root -> right -> right = new Node(6);
+
+  struct Node* root = new Node(1);
+  root -> left = new Node(2);
+  root -> left -> left = new Node(3);
+  root -> left -> left -> right = new Node(4);
+  root -> left -> left -> right -> left = new Node(5);
+  root -> left -> left -> right -> right = new Node(6);
+  root -> right = new Node(7);
+  root -> right -> right = new Node(8);
+  root -> right -> right -> left = new Node(9);
+  root -> right -> right -> left -> left = new Node(10);
+  root -> right -> right -> left -> right = new Node(11);
+
+  
+  vector<int> res;
+  res = boundary(root);
+
+  for(int i =0; i<res.size(); ++i)
+  {
+    cout<<res[i]<<" ";
+  }
+  cout<<endl;
    
 
 	return 0;
