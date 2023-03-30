d361a9d0468f15773b146fc8e4228ae0b23292a7
fucking ajeet <ajeet808080@gmail.com>
2023-03-30 23:31:22 +0530

    update

--- Binary_Search_tree/successor-bst.cpp
+++ Binary_Search_tree/successor-bst.cpp
@@ -18,26 +18,9 @@ struct Node
     
 };
 
-int findceil(Node* root, int key)
-{
-    int ceil = -1;
-    while(root)
-    {
-        if(root->data == key)
-        {
-            ceil = root -> data;
-            return ceil;
-        }
-        if(key > root->data)
-        {
-            root = root->right;
-        }else{
-            ceil = root -> data;
-            root = root->left;
-        }
-    }
-    return ceil;
 
+Node* sucessor(Node* root, Node* key){
+    
 }
 
     
